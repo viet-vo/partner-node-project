be7290878834fd33db41fc5284dd9f6acca120d7
Cisko <cisko_R@hotmail.com>
2020-10-05 00:52:50 -0500

    dont pay attention to this

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-/*
-save this file
-git add .
-git commit - "This is the comment, make it something different for yourself"
-git push origin chico
+const express = require('express')
+const app = express()
+const port = 3000
 
--------------------------------------------------------------------------------
-Try creating another  branch and then merging
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+    res.send('<h2> Chico </h2>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+/* 
+
+Explore post, get, update request (update only read about)
+Try to use moment npm module to ask for the current time.
+Node fs module 
+
+Name input route for express.
+names.json
 */
-
-const object = { a: 2, b: "test" }
-
-const test = () => {
-    console.log("hello")
-};
